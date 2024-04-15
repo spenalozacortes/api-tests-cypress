a23a36be2ea34d584abc73709157dbe4c3dcea8c
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-04-15 15:30:38 -0600

    Complete step 2

--- cypress/e2e/apiTests.cy.js
+++ cypress/e2e/apiTests.cy.js
@@ -12,5 +12,17 @@ describe('API tests', () => {
       const sortedIds = [...ids].sort((a, b) => a - b);
       expect(ids).to.have.ordered.members(sortedIds);
     });
+
+     // Step 2
+    // Send GET request to get post with id=99 (/posts/99)
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/99').then(response => {
+      // Status code is 200
+      expect(response.status).to.eq(200);
+      // Post information is correct: userId is 10, id is 99, title and body arent' empty
+      expect(response.body.userId).to.eq(10);
+      expect(response.body.id).to.eq(99);
+      expect(response.body.title).to.not.be.empty;
+      expect(response.body.body).to.not.be.empty;
+    });   
   });
 });
