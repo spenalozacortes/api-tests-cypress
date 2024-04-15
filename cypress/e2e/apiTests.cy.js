8b8c401efed05eb4d73ab210cc6a9c083157aa7a
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-04-15 15:30:05 -0600

    Complete step 1

--- cypress/e2e/apiTests.cy.js
+++ cypress/e2e/apiTests.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
+describe('API tests', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    // Step 1
+    // Send GET request to get all posts (/posts)
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/posts').then(response => {
+      // Status code is 200
+      expect(response.status).to.eq(200);
+      // The list in response body is JSON
+      expect(response.headers['content-type']).to.include('application/json');
+      // Posts are sorted in ascending order (by id)
+      const ids = response.body.map(post => post.id);
+      const sortedIds = [...ids].sort((a, b) => a - b);
+      expect(ids).to.have.ordered.members(sortedIds);
+    });
+  });
+});
